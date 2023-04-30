290d532acdb2d7db16f453ec328458d94db76d1d
Arash <47273556+arashnrim@users.noreply.github.com>
2023-04-30 18:00:55 +0800

    feat: add erraneous code

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 print("Hello, Git!")
 print("You're running a Python file somewhere inside a Git repository.")
 print("You're likely getting this code from GitHub, too!")
+
+b = False
+if b:
+    print("This line should run!")
