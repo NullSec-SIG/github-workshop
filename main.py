b9abf5ecebd43beb151ceb49b3ca1f7248076bdd
Arash <47273556+arashnrim@users.noreply.github.com>
2023-04-30 18:05:53 +0800

    fix: fix printing logic

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ print("Hello, Git!")
 print("You're running a Python file somewhere inside a Git repository.")
 print("You're likely getting this code from GitHub, too!")
 
-b = False
+b = True
 if b:
     print("This line should run!")
